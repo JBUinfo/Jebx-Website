cb1f1ca35621367272baa09a7bc09684843b87b5
JBUinfo <javierbuinfo@gmail.com>
2022-07-18 19:55:14 +0200

    Added project timeline and search box functionality

---
+++ src/components/word-carrousel/word-carrousel.component.tsx
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+import './word-carrousel.styles.css'
+
+const WordCarrousel = (): JSX.Element => {
+
+  interface IWords{
+    [key: string]: string
+  }// (word) : (css class)
+  const defaultWords1: IWords = {
+    "C#":"word",
+    "NodeJS":"word",
+    "Python":"word",
+    "HTML":"word",
+    "CSS":"word",
+    "C++":"word",
+    "A":"word",
+    "B":"word",
+    "C":"word",
+    "D":"word",
+    "E":"word",
+    "F":"word",
+  }
+  const default1Keys: string[] = Object.keys(defaultWords1);
+  const default1Length: number = default1Keys.length;
+
+  const defaultWords2: IWords = {
+    "Elastic":"word",
+    "Bash":"word",
+    "Socket.io":"word",
+    "PHP":"word",
+    "Angular":"word",
+    "Java":"word",
+    "Docker":"word",
+    "ASM":"word",
+    "A":"word",
+    "B":"word",
+    "C":"word",
+    "D":"word",
+  }
+  const default2Keys: string[] = Object.keys(defaultWords2);
+  const default2Length: number = default2Keys.length;
+  
+  const [query, setQuery] = useState<string>('');
+  const [words1, setWords1] = useState<IWords>(defaultWords1);
+  const [words2, setWords2] = useState<IWords>(defaultWords2);
+
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let values: string[] = event.target.value.trim().toUpperCase().split(",");
+    let Tword1: IWords = {...defaultWords1};
+    let Tword2: IWords = {...defaultWords2};
+    values = values.filter(value => value.trim() !== "");    
+    if(values.length){
+      default1Keys.map( word => {
+        Tword1[word] = "word not-found"
+        values.some((value)=>( word.toUpperCase().includes(value.trim()) && ( Tword1[word] = "word found")))
+        return null;
+      })
+      default2Keys.map( word => {
+        Tword2[word] = "word not-found"
+        values.some((value)=>( word.toUpperCase().includes(value.trim()) && ( Tword2[word] = "word found")))
+        return null;
+      })
+    }
+    setQuery(event.target.value);
+    setWords1(Tword1);
+    setWords2(Tword2);
+  };
+
+  /* With this, any word is going to be duplicated in screen*/
+  const line1 = [...default1Keys, ...default1Keys];//duplicate the array
+  const line2 = [...default2Keys, ...default2Keys];//duplicate the array
+  const line3 = [
+    ...default1Keys.slice(default1Length/2),
+    ...default1Keys.slice(0,default1Length/2),
+    ...default1Keys.slice(default1Length/2),
+    ...default1Keys.slice(0,default1Length/2),
+  ];//half reverse and duplicate
+  const line4 = [
+    ...default2Keys.slice(default2Length/2),
+    ...default2Keys.slice(0,default2Length/2),
+    ...default2Keys.slice(default2Length/2),
+    ...default2Keys.slice(0,default2Length/2),
+  ];//half reverse and duplicate
+  
+
+  return (
+    <>
+    <div className={ 'searchbox' }>
+        <input
+          value={query}
+          onChange={inputHandler}
+          placeholder="C#, NodeJS, React..."
+          className={ "black" }
+        />
+      </div>
+    <div className={ 'carroussel' }>
+          <div className={ 'line left' }>
+          {line1.map((word,i) => <span className={ words1[word] } key={ words1[word] + i }>{word}</span>)}
+          </div>
+          <div className={ 'line right' }>
+          {line2.map((word,i) => <span className={ words2[word] } key={ words2[word] + i }>{word}</span>)}
+          </div>
+          <div className={ 'line left' }>
+          {line3.map((word,i) => <span className={ words1[word] } key={ words1[word] + i }>{word}</span>)}
+          </div>
+          <div className={ 'line right' }>
+          {line4.map((word,i) => <span className={ words2[word] } key={ words2[word] + i }>{word}</span>)}
+          </div>
+      </div>
+      </>
+  );
+};
+  
+export default WordCarrousel;  
--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,96 +1,233 @@
-import { useState } from 'react';
 import TypeIt from "typeit-react";
+import { useState } from 'react';
 
-import './home.styles.css'
-import text_website from '../../assets/text-website.json';
+import "./home.styles.css";
+import img1 from "./imgs/wall1.jpg";
+import text_website from "../../assets/text-website.json";
+import WordCarrousel from "../../components/word-carrousel/word-carrousel.component";
+
 const FirstContainer = (): JSX.Element => {
-    return (<div className={ 'first-container' }>
-      <div className={ 'intro' }>
-        <div className={ 'top' }>
-
+  const leet = new Array(20).fill("L33T");
+  return (
+    <div className={"section-container"}>
+      <div className={"img-filter"}></div>
+      <div className={"img-container"}>
+        <img src={img1} className={"img-example"} alt="" />
+      </div>
+      <div className={"intro"}>
+        <div className={"top"}>
+          <div className={"leet-carrousel"}>
+            {leet.map(()=>(<span>L33T</span>))}
+          </div>
         </div>
-        <div className={ 'main' }>
-          <div className={ 'text' }>
-          <TypeIt
-            getBeforeInit={(instance) => {
-              instance.options({ speed: 50, lifeLike: false }).type(text_website.INTRO_MAIN.en[0])
-              .pause(750).delete(4).type(",").pause(300).break()
-              .type(text_website.INTRO_MAIN.en[1])
-              .pause(300).break()
-              .type("<span class='green'>" + text_website.INTRO_MAIN.en[2] + "</span>")
-              .flush();
-              return instance;
-            }}
-          />
+        <div className={"main"}>
+          <div className={"text"}>
+            <TypeIt
+              getBeforeInit={(instance) => {
+                instance
+                  .options({ speed: 50, lifeLike: false })
+                  .type(text_website.INTRO_MAIN.en[0])
+                  .pause(750)
+                  .delete(4)
+                  .type(",")
+                  .pause(300)
+                  .break()
+                  .type(text_website.INTRO_MAIN.en[1])
+                  .pause(300)
+                  .break()
+                  .type(
+                    "<span class='green'>" +
+                      text_website.INTRO_MAIN.en[2] +
+                      "</span>"
+                  )
+                  .flush();
+                return instance;
+              }}
+            />
+            <br />
+            <br />
+            <div className={"intro-description description"}>asdasd</div>
           </div>
         </div>
       </div>
-    </div>)
+    </div>
+  );
 };
 
 const SecondContainer = (): JSX.Element => {
-  const line1: string[] = ["C#", "NodeJS", "Python", "HTML", "CSS", "C++", "ASM", "C#", "NodeJS", "Python", "HTML", "CSS", "C++", "ASM"] ;
-  const line2: string[] = ["Elastic", "Bash", "Socket.io", "PHP", "Java", "Angular", "Docker", "Elastic", "Bash", "Socket.io", "PHP", "Java", "Angular", "Docker"] ;
-
-  const [query, setQuery] = useState('');
-
-  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
-    
-    setQuery(event.target.value);
-  };
-
-
-  return(<div className={ 'second-container' }>
-    <div className={ 'explanation' }>
-      <div className={ 'title' }>
-        Dedicated to <span className={ 'green' }>EVERYTHING</span>
+  return (
+    <div className={"section-container"}>
+      <div className={"explanation"}>
+        <div className={"title"}>
+          Dedicated to <span className={"blue"}>EVERYTHING</span>
+        </div>
+        <div className={"description"}>
+          I have more than 6 years at IT world and i have been always <br />
+          learning news technologies and languages.
+        </div>
       </div>
-      <div className={ 'description' }>
-      I have more than 6 years at IT world and i have been always <br/>learning news technologies and languages.
+
+      <div className={"center"}>
+        <WordCarrousel />
       </div>
     </div>
+  );
+};
 
-    <div className={ 'center' }>
-      <div className={ 'searchbox' }>
-        <input
-          value={query}
-          onChange={inputHandler}
-          placeholder="C#, NodeJS, React..."
-          className={ "black" }
-        />
+const ThirdContainer = (): JSX.Element => {
+  interface IProject {
+    shortTitle: string,
+    title: string,
+    description: string,
+    date: string
+  }
+  const projects: IProject[] = [
+    {
+      shortTitle: "Twitch",
+      title: "Strools - The Twitch streaming tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+    {
+      shortTitle: "Twitch",
+      title: "Strools - A little Twitch tool",
+      description: "something",
+      date: "10-10-2020"
+    },
+  ]
+  const [information, setInformation] = useState<IProject>();
+
+  return (
+    <div className={"section-container"}>
+      <div className={"left-box"}>
+        <div className={"title"}>
+
+          {information ? (
+            <span className={"orange"}>
+              {information.title}
+            </span>
+            ) : (
+            <>
+              Projects, projects and...
+              <span className={"orange"}> more projects</span>
+            </>
+          )}
+          
+        </div>
+        <div className={"description"}>
+        {information ? information.description : (
+            <>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus harum repellendus commodi dolores eum tempora doloremque. Officiis minima doloribus ipsam reiciendis ducimus vel ad nihil unde enim? Laudantium, eum. Ut!
+            </>
+          )}
+        </div>
       </div>
-      <div className={ 'carroussel' }>
-          <div className={ 'line left' }>
-            {line1.map(word => <span className={ "word" } key={ word }>{word}</span>)}
-            {line1.map(word => <span className={ "word" } key={ word }>{word}</span>)}
+      <div className={"right-box"}>
+        <div className={"center"}>
+          <div style={{width: 80*projects.length+"px"}} className="timeline">
+            <div className="timeline-progress"></div>
+            <div className="timeline-items">
+              {projects.map((project,i)=>(
+                <div className="timeline-item" onClick={(e) => setInformation(project)}>
+                    <div className={"timeline-content "+( 0===i%2 ? "timeline-up" : "timeline-down" )+" description "+ (i===projects.length-1 && "timeline-last-item")}>{project.shortTitle}</div>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={ 'line right' }>
-            {line2.map(word => <span className={ "word" } key={ word }>{word}</span>)}
-            {line2.map(word => <span className={ "word" } key={ word }>{word}</span>)}
-          </div>
-          <div className={ 'line left' }>
-            {line1.reverse().map(word => <span className={ "word" } key={ word }>{word}</span>)}
-            {line1.reverse().map(word => <span className={ "word" } key={ word }>{word}</span>)}
-          </div>
-          <div className={ 'line right' }>
-            {line2.reverse().map(word => <span className={ "word" } key={ word }>{word}</span>)}
-            {line2.reverse().map(word => <span className={ "word" } key={ word }>{word}</span>)}
-          </div>
+        </div>
       </div>
     </div>
-  </div>)
+  );
 };
 
 const HomePage = (): JSX.Element => {
   return (
-    <div className={ 'home-container' }>
+    <div className={"home-container"}>
       <>
         {FirstContainer()}
         {SecondContainer()}
+        {ThirdContainer()}
       </>
     </div>
   );
 };
-  
-export default HomePage;  
+
+export default HomePage;
